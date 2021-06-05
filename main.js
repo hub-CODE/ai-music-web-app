630b2c49796cd5af83d32bd7d8452ce6758793ea
hub-CODE <66951180+hub-CODE@users.noreply.github.com>
2021-06-05 12:22:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song1 = "";
 song2 = "";
 
+leftWristX = "0";
+leftWristY = "0";
+
+rightWristX = "0";
+rightWristY = "0";
+
 function preload(){
     song1.loadSound("Alone.mp3");
     song2.loadSound("OnMyWay.mp3");
@@ -12,7 +18,32 @@ function setup() {
     
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded(){
+  console.log("Posenet is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+      console.log(results);
+      scoreLeftWrist =  results[0].pose.keypoints[9].score;
+      console.log("scoreLeftWristX = " + scoreLeftWrist);
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+      
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
+
 function draw() {
     image(video, 0, 0, 600, 500);
 }
